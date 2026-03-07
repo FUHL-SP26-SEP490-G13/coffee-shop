2610d6cf84835a9364a2875fc0d5cad5c0753101
Chu Thế Văn <chuthevan450@gmail.com>
2026-03-07 11:13:33 +0700

    Update areaService.js

--- coffee-shop-frontend/src/services/areaService.js
+++ coffee-shop-frontend/src/services/areaService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { API_ENDPOINTS, STORAGE_KEYS } from "../constants";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 const areaService = {
   /**
