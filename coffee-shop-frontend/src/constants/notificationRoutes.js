acce5324699d1d3198e6f009010ad1b2ef7aef69
tiennthe <192111087+tiennthe@users.noreply.github.com>
2026-03-31 22:29:09 +0700

    git

--- coffee-shop-frontend/src/constants/notificationRoutes.js
+++
@@ -1,8 +0,0 @@
-export const NOTIFICATION_ROUTES = {
-  newsletter: "/admin/subscriber",
-//   order: "/admin/orders",
-//   user: "/admin/users",
-//   banner: "/admin/banners",
-//   area: "/admin/area",
-//   table: "/admin/tables",
-};
