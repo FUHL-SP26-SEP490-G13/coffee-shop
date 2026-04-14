1969b6f416125ec93d7d3739494102cfc66de84b
tiennthe <192111087+tiennthe@users.noreply.github.com>
2026-04-14 23:04:35 +0700

    add shift report

--- coffee-shop-frontend/src/routes/AppRoutes.jsx
+++ coffee-shop-frontend/src/routes/AppRoutes.jsx
@@ -56,6 +56,7 @@ import AdminReputation from "@/pages/admin/AdminReputation/AdminReputation";
 import AdminLoyalty from "@/pages/admin/AdminLoyalty/AdminLoyalty";
 
 import AdminEndOfDayReport from "@/pages/admin/AdminEndOfDayReport/AdminEndOfDayReport";
+import AdminShiftReport from "@/pages/admin/AdminShiftReport/AdminShiftReport";
 import TakeawayPOS from '../pages/staff/TakeawayPOS'
 import { OrderDelivery } from '@/pages/staff/StaffOrderList';
 import { StaffDashboard } from "@/pages/staff/StaffDashboard/StaffDashboard";
@@ -225,6 +226,7 @@ const AppRoutes = () => {
         <Route index element={<Navigate to="dashboard" replace />} />
         <Route path="dashboard" element={<AdminDB />} />
         <Route path="end-of-day-report" element={<AdminEndOfDayReport />} />
+        <Route path="shift-report" element={<AdminShiftReport />} />
         <Route path="menu/products" element={<AdminProducts />} />
         <Route path="menu/categories" element={<AdminCategories />} />
         <Route path="orders" element={<AdminOrders />} />
